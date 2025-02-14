93e518dadbbc16bd71ef8d277efadc99b6e80ac8
jargalmaaaaa19 <gegeej7@gmail.com>
2025-02-14 17:55:39 +0800

    hbui

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -1,13 +1,21 @@
-package edu.cmu.cs.cs214.lab02.shapes;
+package edu.cmu.cs.cs214.lab02.shapes.implementations;
+
+import edu.cmu.cs.cs214.lab02.shapes.base.Shape;
 
 public class Square implements Shape {
-    public double sideLen;
+    private double sideLen;
 
     public Square(double sideLen) {
         this.sideLen = sideLen;
     }
-    
+
+    @Override
     public double getArea() {
         return sideLen * sideLen;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a square with side length " + sideLen);
     }
 }
